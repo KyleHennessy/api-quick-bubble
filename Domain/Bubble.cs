928fdb2a173807124b45c5990ed5a9790aeffb30
Kyle Hennessy <kyle-hennessy@hotmail.com>
2024-08-16 00:48:06 +0100

    Add project files.

---
+++ Domain/Bubble.cs
@@ -0,0 +1,13 @@
+﻿namespace Domain
+{
+    public class Bubble
+    {
+        public string Message { get; set; } = null!;
+        public DateTime RemoveAt { get; set; } = DateTime.Now.AddSeconds(20);
+
+        public string InitialX { get; set; }
+        public string InitialY { get; set; }
+        public string FinalX { get; set; }
+        public string FinalY { get; set; }
+    }
+}
---
+++ api-quick-bubble/Hubs/BubbleHub.cs
@@ -0,0 +1,13 @@
+﻿using Domain;
+using Microsoft.AspNetCore.SignalR;
+
+namespace api_quick_bubble.Hubs
+{
+    public class BubbleHub : Hub
+    {
+        public async Task SendMessage(Bubble bubble)
+        {
+            await Clients.Others.SendAsync("ReceiveMessage", bubble);
+        }
+    }
+}

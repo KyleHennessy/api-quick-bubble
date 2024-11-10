254aee0330bbf52fd41b34eca94c7f7de34aeafa
Kyle Hennessy <kyle-hennessy@hotmail.com>
2024-11-10 19:05:42 +0000

    Removed Coordinate  properties from the Bubble model as no longer planning to add this as a feature

--- Domain/Bubble.cs
+++ Domain/Bubble.cs
@@ -2,15 +2,8 @@
 {
     public class Bubble
     {
-        private static readonly Random rand = new Random();
-
         public Guid Id { get; set; } = Guid.NewGuid();
         public string Message { get; set; } = null!;
         public string Colour { get; set; } = null!;
-        public DateTime RemoveAt { get; set; } = DateTime.Now.AddSeconds(20);
-        public int InitialX { get; set; } = rand.Next(1, 301);
-        public int InitialY { get; set; } = rand.Next(1, 301);
-        public int FinalX { get; set; } = rand.Next(1, 101);
-        public int FinalY { get; set; } = rand.Next(1, 101);
     }
 }

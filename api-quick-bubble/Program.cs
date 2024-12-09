c6a5defa336712742f0a137dbee7bdaf8c47a56d
kyle hennessy <kyle-hennessy@hotmail.com>
2024-12-09 20:07:41 +0000

    Enabling swaggerUI when release is production to use it in the azure web app

--- api-quick-bubble/Program.cs
+++ api-quick-bubble/Program.cs
@@ -21,7 +21,7 @@ builder.Services.AddTransient<IImageCompressor, ImageCompressor>();
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
+if (app.Environment.IsDevelopment() || app.Environment.IsProduction())
 {
     app.UseSwagger();
     app.UseSwaggerUI();

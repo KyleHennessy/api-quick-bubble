9ad60259ccab6ee0a26f49a0c2795c6d45ee600d
kyle hennessy <kyle-hennessy@hotmail.com>
2025-03-20 23:35:49 +0000

    Added max length and regex validation to bubbles

--- Domain/Bubble.cs
+++ Domain/Bubble.cs
@@ -1,10 +1,17 @@
-﻿namespace Domain
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Domain
 {
     public class Bubble
     {
         public Guid Id { get; set; } = Guid.NewGuid();
+
+        [MaxLength(250, ErrorMessage = "Messages can be no more than 250 characters long")]
         public required string Message { get; set; } = null!;
+
+        [RegularExpression("^#([A-Fa-f0-9]{6}|[A-Fa-f0-9]{3})$", ErrorMessage = "This is not a valid hexadecimal colour code")]
         public required string Colour { get; set; } = null!;
+
         public string? Background { get; set; }
     }
 }
